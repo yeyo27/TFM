50181d45227857db089a89acfd8ef8fefe6add7c
yeyo27 <yeyo1mm@gmail.com>
2023-12-25 19:01:50 +0100

    Added complete OAuth2 funtionality with JWT and password hashing

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from os import getenv
 from pathlib import Path
 from typing import Annotated
 
 from fastapi import FastAPI, UploadFile, File, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel, HttpUrl, EmailStr
 from src.text_scraping import HtmlCleaner, PyMuPdfCleaner, get_readable_html
 from src.embeddings_calculator import EmbeddingsCalculator
@@ -25,19 +25,79 @@ fake_users_db = {
     "johndoe": {
         "username": "johndoe",
         "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",  # secret
+        "created_at": datetime.utcnow(),
     },
     "alice": {
         "username": "alice",
-        "full_name": "Alice Wonderson",
-        "hashed_password": "fakehashedsecret2",
+        "email": "alice@example.com",
+        "hashed_password": "$2b$12$yF8xd7gjmZfec4Z/wKYbXudFo0EVeqAJ5xPSzKXJdTRYi2rIVaoEq",  # secret2
+        "created_at": datetime.utcnow(),
     },
+    "johndoe@example.com": "johndoe",
+    "alice@example.com": "alice",
+}
+
+fake_history_db = {
+    "johndoe":
+        [
+            {
+                'id': 1,
+                'sourceName': 'url1.com',
+                'date': '2022-01-01 12:00:00',
+                'query': 'What is the weather like?',
+                'response': 'The weather is sunny',
+            },
+            {
+                'id': 2,
+                'sourceName': 'url2.com',
+                'date': '2031-01-01 12:00:00',
+                'query': 'What is the weather like?',
+                'response': 'The weather is nothing',
+            },
+            {
+                'id': 4,
+                'username': 'johndoe',
+                'sourceName': 'pdf1',
+                'date': '2022-01-01 12:00:00',
+                'query': 'What is the weather like?',
+                'response': 'The weather is sunny',
+            },
+        ],
+    "alice":
+        [
+            {
+                'id': 3,
+                'username': 'alice',
+                'sourceName': 'url3.com',
+                'date': '2022-01-01 12:00:00',
+                'query': 'What is the sñkfñodfksd like?',
+                'response': 'The weather is skejfoiw',
+            },
+            {
+                'id': 5,
+                'username': 'alice',
+                'sourceName': 'pdf3',
+                'date': '2022-01-01 12:00:00',
+                'query': 'What is the weather like?',
+                'response': 'The weather is sunny',
+            }
+        ],
 }
 
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+
 class NewUser(BaseModel):
+    username: str
     email: EmailStr
-    name: str
     password: str
     confirm_password: str
 
@@ -45,7 +105,11 @@ class NewUser(BaseModel):
 class User(BaseModel):
     username: str
     email: EmailStr
-    name: str
+
+
+class UserInDb(User):
+    hashed_password: str
+    created_at: datetime
 
 
 class ArticleUrl(BaseModel):
@@ -59,9 +123,17 @@ class HealthCheckResponse(BaseModel):
     status: str
 
 
-app = FastAPI()
+calculator = EmbeddingsCalculator()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+database = VectorDB()
+
+generator = QuestionGeneratorTransformers()
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/v1/token")
+
+app = FastAPI()
 
 origins = [
     "http://localhost:3000",
@@ -75,22 +147,71 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-calculator = EmbeddingsCalculator()
 
-database = VectorDB()
-
-generator = QuestionGeneratorTransformers()
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
 
 
-def fake_decode_token(token):
-    return User(
-        username=token + "fakedecoded", email="john@example.com", name="John Doe"
-    )
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDb(**user_dict)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
+    credentials_exception = HTTPException(
+            status_code=401,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if not username:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if not user:
+        raise credentials_exception
     return user
+
+
+def insert_user_to_fake_db(user_data: NewUser):
+    # store credentials in db
+    fake_users_db[user_data.username] = {
+        "username": user_data.username,
+        "email": user_data.email,
+        "hashed_password": get_password_hash(user_data.password),
+        "created_at": datetime.utcnow(),
+    }
+    fake_users_db[user_data.email] = user_data.username
+    return fake_users_db[user_data.username]
 
 
 @app.get("/")
@@ -104,31 +225,34 @@ async def root():
     return {"message": "TFM API v1.0"}
 
 
-@app.post("/api/v1/login")
-async def login(credentials: User):
-    user_exists = True
-    if not user_exists:
-        raise HTTPException(status_code=401, detail="Invalid credentials")
-    return {"message": "User logged in", "credentials": credentials}
+@app.post("/api/v1/token", response_model=Token)
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=401,
+                            detail="Incorrect username or password",
+                            headers={"WWW-Authenticate": "Bearer"})
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 @app.post("/api/v1/signup")
 async def signup(user_data: NewUser):
+    if user_data.username in fake_users_db:
+        raise HTTPException(status_code=400, detail="Username already exists")
+    if user_data.email in fake_users_db:
+        raise HTTPException(status_code=400, detail="Email already exists")
     if user_data.password != user_data.confirm_password:
         raise HTTPException(status_code=400, detail="Passwords do not match")
     # store credentials in db
-    new_user = {
-        "email": user_data.email,
-        "name": user_data.name,
-        "password": user_data.password,  # maybe hash it
-        "created_at": datetime.now(),
-    }
+    new_user = insert_user_to_fake_db(user_data)
     return {"message": "User created successfully", "new_user": new_user}
 
 
 @app.get("/api/v1/history")
-def history(token: Annotated[str, Depends(oauth2_scheme)]):
-    return {"token": token}
+async def history(current_user: Annotated[User, Depends(get_current_user)]):
+    return current_user
 
 
 @app.post("/api/v1/url")
